35f633e8cafacdf0e5c6c305a14c79de1d1b27ab
Frank Li <frankvsense@gmail.com>
2015-08-16 01:37:50 -0400

    Initial setup

---
+++ Hanoi-swift/Controllers/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Hanoi-swift
+//
+//  Created by Frank Li on 8/16/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Hanoi-swift/Models/GameLogic.swift
@@ -0,0 +1,13 @@
+//
+//  GameLogic.swift
+//  Hanoi-swift
+//
+//  Created by Frank Li on 8/16/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+import Foundation
+
+class GameLogic: NSObject {
+   
+}
