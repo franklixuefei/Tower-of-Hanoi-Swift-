608ef5e671d1453b35b5fb5a5281f48b9751c880
Frank Li <frankvsense@gmail.com>
2015-08-16 01:31:26 -0400

    Initial Commit

---
+++ Hanoi-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hanoi-swift
+//
+//  Created by Frank Li on 8/16/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Hanoi-swiftTests/Hanoi_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hanoi_swiftTests.swift
+//  Hanoi-swiftTests
+//
+//  Created by Frank Li on 8/16/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Hanoi_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
