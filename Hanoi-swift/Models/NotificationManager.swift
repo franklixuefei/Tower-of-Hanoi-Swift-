d3df8b04107075390babcca0513d0cbf4ab79756
Frank Li <frankvsense@gmail.com>
2015-08-23 17:08:42 -0400

    Updates NotificationManager

--- Hanoi-swift/Models/NotificationManager.swift
+++ Hanoi-swift/Models/NotificationManager.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class NotificationManager {
+  static let defaultManager = NotificationManager()
+  lazy private var observerTokens: [AnyObject] = []
+  lazy private var operationQueue: NSOperationQueue = {
+    var queue = NSOperationQueue()
+    queue.name = "Block Queue"
+    return queue
+  }()
+  
+  deinit {
+    deregisterAll()
+  }
+  
+  private func deregisterAll() {
+    for token in observerTokens {
+      NSNotificationCenter.defaultCenter().removeObserver(token)
+    }
+    observerTokens = []
+  }
+  
+  func registerObserver(name: String!, block: (NSNotification!) -> Void) {
+    let newToken = NSNotificationCenter.defaultCenter().addObserverForName(name, object: nil, queue: operationQueue) {
+      note in block(note)
+    }
+    
+    observerTokens.append(newToken)
+  }
+  
+  func registerObserver(name: String!, forObject object: AnyObject!, block: (NSNotification!) -> Void) {
+    let newToken = NSNotificationCenter.defaultCenter().addObserverForName(name, object: object, queue: operationQueue)
+      {note in block(note)}
+    
+    observerTokens.append(newToken)
+  }
+}
